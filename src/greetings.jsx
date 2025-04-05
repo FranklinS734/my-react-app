17a09b647d8ece3581f81a4869ef9a70c73e9289
FranklinS734 <martinc3@usf.edu>
2025-04-05 00:38:21 -0400

    component creation and integration

---
+++ src/greetings.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Greeting() {
+  return (  
+
+<div>
+  <h2>Hello from the Greeting Component!</h2>
+  <p>This is your first custom component in React.
+     Can't wait for next week, SPX 4000!
+  </p>
+</div>
+    );
+}
+export default Greeting;
